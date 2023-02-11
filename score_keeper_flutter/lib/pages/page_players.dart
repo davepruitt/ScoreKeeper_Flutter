bfb04c33c78450be2be234c386e8b215bbefd019
David Pruitt <15479248+davepruitt@users.noreply.github.com>
2023-02-10 23:46:47 -0700

    Working on the players page

--- score_keeper_flutter/lib/pages/page_players.dart
+++ score_keeper_flutter/lib/pages/page_players.dart
@@ -11,6 +11,44 @@ class Page_Players extends StatefulWidget
 
 class Page_Players_State extends State<Page_Players> 
 {
+    void clear_players ()
+    {
+        setState(() 
+        {
+
+        });
+    }
+
+    void add_player () 
+    {
+        setState(() 
+        {
+            //empty
+        });
+    }
+
+    Widget build_single_player (String player_name)
+    {
+        Row result = Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+                TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Player name"
+                    ),
+                    controller: TextEditingController(text: player_name),
+                ),
+                ElevatedButton(
+                    onPressed: () { },
+                    child: Icon(Icons.clear),
+                ),
+            ]
+        );
+
+        return result;
+    }
+
     @override
     Widget build(BuildContext context) 
     {
@@ -19,10 +57,27 @@ class Page_Players_State extends State<Page_Players>
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                        const Text('This is the players page'),
+                            build_single_player("Hello, World!"),
                         ],
                 ),
-            )
+            ),
+            floatingActionButton: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                    FloatingActionButton(
+                        onPressed: clear_players,
+                        tooltip: 'Clear al players',
+                        child: const Icon(Icons.delete),
+                    ),
+                    SizedBox(width: 20),
+                    FloatingActionButton(
+                        onPressed: add_player,
+                        tooltip: 'Add a player',
+                        child: const Icon(Icons.add),
+                    )
+                ]
+            ),
         );
     }
+
 }
