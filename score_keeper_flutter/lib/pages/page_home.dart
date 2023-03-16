e3b1fdb065eedd568547b5149b46d4417ba2c297
David Pruitt <15479248+davepruitt@users.noreply.github.com>
2023-03-16 11:29:59 -0600

    Create a basic display of all the current scores on the main page of the app. It's functional, but I still need to make sure scrolling in both directions works properly. I also need to enable functionality to transpose the scores table (put people in columns rather than rows)

--- score_keeper_flutter/lib/pages/page_home.dart
+++ score_keeper_flutter/lib/pages/page_home.dart
@@ -43,6 +43,89 @@ class Page_Home_State extends State<Page_Home>
         );
     }
 
+    List<DataColumn> build_data_table_columns (BuildContext context)
+    {
+        //Create an empty list of columns
+        List<DataColumn> columns = <DataColumn>[];
+
+        //Create a column for each round of the game
+        if (globals.app_scoresheet.players.isNotEmpty)
+        {
+            //Create the name column
+            DataColumn name_column = const DataColumn(label: Text("Name"));
+            columns.add(name_column);
+
+            if (globals.app_scoresheet.players[0].scores.isNotEmpty)
+            {
+                int num_rounds = globals.app_scoresheet.players[0].scores.length;
+                for (int i = 0; i < num_rounds; i++)
+                {
+                    DataColumn current_round_column = DataColumn(
+                        label: Text(i.toString())
+                    );
+
+                    columns.add(current_round_column);
+                }
+
+                //Create a "sum" column at the end
+                DataColumn sum_column = const DataColumn(label: Text("Sum"));
+                columns.add(sum_column);
+            }
+        }
+
+        return columns;
+    }
+
+    List<DataRow> build_data_table_rows (BuildContext context)
+    {
+        //Create a list of rows that we will return to the caller
+        List<DataRow> rows = <DataRow>[];
+
+        //Iterate over each player
+        for (int i = 0; i < globals.app_scoresheet.players.length; i++)
+        {
+            //Create a list of DataCell objects for the current player
+            List<DataCell> row_cells = <DataCell>[];
+
+            //Add the player's name as the first column of each row
+            DataCell name_cell = DataCell(Text(globals.app_scoresheet.players[i].name));
+            row_cells.add(name_cell);
+
+            //Add each round for this player to the list of cells
+            for (int j = 0; j < globals.app_scoresheet.players[i].scores.length; j++)
+            {
+                DataCell current_round_cell = DataCell(Text(globals.app_scoresheet.players[i].scores[j].round().toString()));
+                row_cells.add(current_round_cell);
+            }
+
+            //If there is at least one round of scores, go ahead and tack on a "sum" column
+            if (globals.app_scoresheet.players[i].scores.isNotEmpty)
+            {
+                DataCell sum_cell = DataCell(Text(globals.app_scoresheet.players[i].get_summed_scores().round().toString()));
+                row_cells.add(sum_cell);
+            }
+
+            //Now create a DataRow object to hold all the DataCell objects
+            DataRow current_row = DataRow(
+                cells: row_cells
+            );
+
+            //Now add the current row to the list of all rows
+            rows.add(current_row);
+        }
+
+        //Return the list of rows
+        return rows;
+    }
+
+    Widget build_data_table (BuildContext context)
+    {
+        return DataTable(
+            columns: build_data_table_columns(context),
+            rows: build_data_table_rows(context)
+        );
+    }
+
     @override
     Widget build(BuildContext context) 
     {
@@ -51,7 +134,9 @@ class Page_Home_State extends State<Page_Home>
             body: SingleChildScrollView(
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [],
+                    children: [
+                        build_data_table(context)
+                    ],
                 ),
             ),
             floatingActionButton: Row(
@@ -63,14 +148,14 @@ class Page_Home_State extends State<Page_Home>
                         tooltip: 'Clear all rounds',
                         child: const Icon(Icons.delete),
                     ),
-                    SizedBox(width:20),
+                    const SizedBox(width:20),
                     FloatingActionButton(
                         heroTag: "remove_round_button",
                         onPressed: ask_user_about_removal_of_round,
                         tooltip: 'Remove most recent round',
                         child: const Icon(Icons.remove)
                     ),
-                    SizedBox(width: 20),
+                    const SizedBox(width: 20),
                     FloatingActionButton(
                         heroTag: "add_round_button",
                         onPressed: add_new_round,
